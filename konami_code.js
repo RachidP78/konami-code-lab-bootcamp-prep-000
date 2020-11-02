233991e1d4fdf6005591834a0b1f7ac75e4f8b92
Rachid Abu-Hassan <rachid@platform78.com>
2020-11-02 07:04:46 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,18 +15,16 @@ let index = 0
 function init() {
   document.body.addEventListener("keydown", (event) => {
     const keyName = event.key
-
     if (key === codes[index]) {
       index++
       if (index === codes.length) {
         alert("You have done it!!!")
         index = 0
-        }
       } else {
         index = 0
-      }
+      } 
+    }
   }
 }
-
 // KeyboardEvent.key
 // KeyboardEvent()
